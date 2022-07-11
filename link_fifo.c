08aca518d0478a0caa4b232ae7308d27afecd5fc
Logan Kenwright <lken274@aucklanduni.ac.nz>
2022-07-11 13:54:19 +1200

    fixed a couple of control logic bugs

--- link_fifo.c
+++ link_fifo.c
@@ -8,6 +8,7 @@ typedef struct {
     size_t head;
     size_t tail;
     size_t size;
+    uint16_t num_elements;
     LinkFrame* data;
 } LinkQueue;
 
@@ -15,6 +16,7 @@ typedef struct {
 LinkFrame link_queue_read(LinkQueue* q) {
     LinkFrame handle = q->data[q->tail];
     q->tail = (q->tail + 1) % q->size;
+    q->num_elements--;
     return handle;
 }
 
@@ -29,6 +31,7 @@ LinkFrame link_queue_peek(LinkQueue* q) {
 }
 
 double link_get_tail_time(LinkQueue* q){
+    if (q->num_elements == 0) return DBL_MAX;
     LinkFrame handle = q->data[q->tail];
     return handle.scheduledTime;
 }
@@ -39,6 +42,7 @@ int link_queue_write(LinkQueue* q, LinkFrame handle) {
     }
     q->data[q->head] = handle;
     q->head = (q->head + 1) % q->size;
+    q->num_elements++;
     return 0;
 }
 
@@ -48,6 +52,7 @@ LinkQueue link_make_queue(int size) {
     q.head=0;
     q.tail=0;
     q.size=size;
+    q.num_elements = 0;
     q.data= malloc(sizeof(LinkFrame) * size);
     return q;
 }
